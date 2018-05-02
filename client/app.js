5a51cbf82d5e34d1f87861bc107fe52c44312bfd
Jonathan Theismann <jmt521@gmail.com>
2018-05-02 00:01:13 -0400

    Add node client and start of scoreboard

--- client/app.js
+++
@@ -1,8 +0,0 @@
-var SerialPort = require("serialport");
-var serialport = new SerialPort("COM3");
-serialport.on('open', function(){
-  console.log('Serial Port Opend');
-  serialport.on('data', function(data){
-      console.log(data);
-  });
-});
